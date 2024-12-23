bed6a485c50c8f031b75e7f5821db045dfcffce9
ManShunChoong <72306346+ManShunChoong@users.noreply.github.com>
2024-12-23 17:46:49 +0800

    add django silk + upload pipfile

--- system/settings.py
+++ system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
+    "silk",
     "inventory",
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "silk.middleware.SilkyMiddleware",
 ]
 
 ROOT_URLCONF = "system.urls"
